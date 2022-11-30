7e15f2bdbf2632f3e0ab85404897debf42981b5f
JungWook Lee <murf@vlending.co.kr>
2022-11-30 11:17:59 +0900

    Initial commit

---
+++ Example/AdServiceManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AdServiceManager
+//
+//  Created by ljy8509113@nate.com on 11/30/2022.
+//  Copyright (c) 2022 ljy8509113@nate.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
